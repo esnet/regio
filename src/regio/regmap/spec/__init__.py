ff37970f80bf9f8b3cb11fe17ed78b84bcd8c737
Jeremy Ranger <jranger@es.net>
2024-01-20 08:30:59 -0500

    regmap: Break up automatic import chain to avoid pulling on too much
    This change reduces what is imported via the top-level __init__.py file to stop
    importing the world. Instead, users should simply import the sub-packages they
    need explicitly.

--- src/regio/regmap/spec/__init__.py
+++ src/regio/regmap/spec/__init__.py
@@ -1,8 +1,17 @@
 #---------------------------------------------------------------------------------------------------
+__all__ = (
+    'AddressSpace',
+    'Array',
+    'Access',
+    'Field',
+    'Register',
+    'Structure',
+    'Union',
+)
+
 from .address import AddressSpace
 from .array import Array
 from .field import Access, Field
-from .info import *
 from .register import Register
 from .structure import Structure
 from .union import Union
